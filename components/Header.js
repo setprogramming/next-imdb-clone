2f353d4a57239a0c73564373293b6d8e32bc4aa7
Set Programming <setprogramming@gmail.com>
2022-05-05 11:52:28 +0300

    styled the header section

--- components/Header.js
+++ components/Header.js
@@ -5,8 +5,8 @@ import { HomeIcon, UserIcon, PhoneIcon, InformationCircleIcon } from '@heroicons
 
 export default function Header() {
   return (
-    <div>
-        <div className="">
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+        <div className="flex">
             <HeaderIcon Icon={HomeIcon} title="HOME" />
             <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
             <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
@@ -16,6 +16,7 @@ export default function Header() {
             src="https://static.wikia.nocookie.net/logopedia/images/8/83/IMDb_Logo.svg/revision/latest/scale-to-width-down/300?cb=20220306043925"
             width={100}
             height={50}
+            className="cursor-pointer active:brightness-110"
         />
     </div>
   )
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -2,9 +2,9 @@
 
 export default function HeaderIcon({Icon, title}) {
   return (
-    <div>
+    <div className="mx-4 flex flex-col hover:text-white active:text-red-500 cursor-pointer lg:mx-6">
         <Icon className="h-8" />
-        <p>{title}</p>
+        <p className="my-2">{title}</p>
     </div>
   )
 }
