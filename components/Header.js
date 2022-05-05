8fca98e38396966b4702d958c29997da4b55aedb
Set Programming <setprogramming@gmail.com>
2022-05-05 11:16:39 +0300

    created the header component

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import HeaderIcon from "./HeaderIcon";
+import { HomeIcon, UserIcon, PhoneIcon, InformationCircleIcon } from '@heroicons/react/solid';
 
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <div className="">
+            <HeaderIcon Icon={HomeIcon} title="HOME" />
+            <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
+            <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
+            <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
+        </div>
+        <Image 
+            src="https://static.wikia.nocookie.net/logopedia/images/8/83/IMDb_Logo.svg/revision/latest/scale-to-width-down/300?cb=20220306043925"
+            width={100}
+            height={50}
+        />
+    </div>
   )
 }
---
+++ components/HeaderIcon.js
@@ -0,0 +1,10 @@
+
+
+export default function HeaderIcon({Icon, title}) {
+  return (
+    <div>
+        <Icon className="h-8" />
+        <p>{title}</p>
+    </div>
+  )
+}
